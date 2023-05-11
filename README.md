cf930cd095513c64b73cab96569174aca8c3bba3
Leonardo Ledda <leonardoledda@gmail.com>
2023-05-11 17:15:40 +0000

    Add extras for sunfish-user 13 TQ2A.230505.002 9891397 release-keys
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

---
+++ README.md
@@ -0,0 +1,14 @@
+# sunfish-user 13 TQ2A.230505.002 9891397 release-keys
+- manufacturer: google
+- platform: sm6150
+- codename: sunfish
+- flavor: sunfish-user
+- release: 13
+- id: TQ2A.230505.002
+- incremental: 9891397
+- tags: release-keys
+- fingerprint: google/sunfish/sunfish:13/TQ2A.230505.002/9891397:user/release-keys
+- is_ab: true
+- brand: google
+- branch: sunfish-user-13-TQ2A.230505.002-9891397-release-keys
+- repo: google_sunfish_dump
---
+++ twrp-device-tree/google/sunfish/omni_sunfish.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from sunfish device
+$(call inherit-product, device/google/sunfish/device.mk)
+
+PRODUCT_DEVICE := sunfish
+PRODUCT_NAME := omni_sunfish
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 4a
+PRODUCT_MANUFACTURER := google
+
+PRODUCT_GMS_CLIENTID_BASE := android-google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="sunfish-user 13 TQ2A.230505.002 9891397 release-keys"
+
+BUILD_FINGERPRINT := google/sunfish/sunfish:13/TQ2A.230505.002/9891397:user/release-keys
